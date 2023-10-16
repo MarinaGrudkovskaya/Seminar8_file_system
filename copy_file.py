d4fd0231d7dddaecf199f86c1df5bbab0b74b142
Marina <137428891+MarinaGrudkovskaya@users.noreply.github.com>
2023-10-16 17:15:31 +0300

    Update copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -1,1 +1,11 @@
 
+
+
+def choice_number_file():
+    print_file()
+    number = int(input("Выберит файл, с которым Вы хотите работать\n"
+                       "Введите цифру 1 или 2: "))
+    while number < 1 or number > 2:
+        number = int(input("Ошибка!!!\n"
+                           "Введите цифру 1 или 2: "))
+    return number
